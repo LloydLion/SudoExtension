f286e580729c8c1e892cb8ec8fd6d9660ba2dcfc
LloydLion <LloydLion2211@gmail.com>
2020-08-21 01:13:09 +0300

    Добавьте файлы проекта.

---
+++ SudoExtension/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net.Cache;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SudoExtension
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string command;
+			string[] comArgs = new string[args.Length - 1];
+			Array.Copy(args, 1, comArgs, 0, comArgs.Length);
+
+			if(File.Exists(Environment.CurrentDirectory + Path.DirectorySeparatorChar + args[0]))
+			{
+				command = Environment.CurrentDirectory + Path.DirectorySeparatorChar + args[0];
+			}
+			else
+			{
+				command = args[0];
+			}
+
+			var process = Process.Start(new ProcessStartInfo()
+			{
+				FileName = command,
+				Arguments = string.Join(" ", comArgs),
+				WorkingDirectory = Environment.CurrentDirectory,
+				Verb = "runas",
+				UseShellExecute = true
+			});
+
+			process.WaitForExit();
+		}
+	}
+}
